8ad92dd1160ae0adaaca7d2f7aac9d2b4547809f
Craig Buchanan <craig.buchanan@cogniza.com>
2022-12-02 16:08:50 +0000

    initial commit

---
+++ Get-customMedical.py
@@ -0,0 +1,7 @@
+import os
+import custom_table
+
+# get the customMedical table for employee # 247
+table = custom_table.get_table(247, 'customMedical', os.environ['BAMBOOHR_API_KEY'])
+
+print(table)
---
+++ custom_table.py
@@ -0,0 +1,39 @@
+import requests
+import json
+import pandas as pd
+
+#
+# GET data from an employee table
+#
+
+def get_table(employee_id: int, table_name: str, token: str):
+    """Gets the data from the specified table for the specified employee."""
+
+    # string interpolation
+    uri = f'https://api.bamboohr.com/api/gateway.php/lorenzbus/v1/employees/{employee_id}/tables/{table_name}'
+
+    # 10 seconds
+    response = requests.get(uri, auth=(token,'password'), headers={'content-type': 'application/json', 'accept': 'application/json'}, timeout=10)
+
+    # print(type(response.json()))
+    # return response.json()[0]
+
+    # convert to dataframe
+    return pd.DataFrame(response.json())
+
+#
+# POST data to an employee table
+#
+
+def set_table(employee_id: int, table_name: str, data: dict, token: str):
+    """Sets the data from the specified table for the specified employee."""
+
+    # string interpolation
+    uri = f'https://api.bamboohr.com/api/gateway.php/lorenzbus/v1/employees/{employee_id}/tables/{table_name}'
+    
+    # convert dictionary to JSON
+    payload = json.dumps(data)
+
+    # 10 seconds
+    response = requests.post(uri,  auth=(token,'password'), json=payload, headers={'content-type': 'application/json'}, timeout=10)
+    print(response.status_code)
