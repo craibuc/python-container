13396c3384bec87fdb6cc329fc601e2de4011d9c
Craig Buchanan <craig.buchanan@cogniza.com>
2022-12-02 16:38:58 +0000

    adding subdomain

--- Get-customMedical.py
+++ Get-customMedical.py
@@ -2,6 +2,6 @@ import os
 import custom_table
 
 # get the customMedical table for employee # 247
-table = custom_table.get_table(247, 'customMedical', os.environ['BAMBOOHR_API_KEY'])
+table = custom_table.get_table(247, 'customMedical', os.environ['BAMBOOHR_API_KEY'], os.environ['BAMBOOHR_SUBDOMAIN'])
 
 print(table)
